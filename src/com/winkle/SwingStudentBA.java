02f79f56f6c72a537bd6b5274dbebfd58a431b56
james <jwin4740@gmail.com>
2018-05-04 21:22:42 -0400

    rendering with borderlayout and titled border

--- src/com/winkle/SwingStudentBA.java
+++ src/com/winkle/SwingStudentBA.java
@@ -18,10 +18,11 @@ public class SwingStudentBA extends JPanel
     JButton openButton, saveButton;
     JTextArea log;
     JFileChooser fc;
-
+    Border paddingBorder = BorderFactory.createEmptyBorder(50, 15, 50, 15);
 
     public SwingStudentBA() {
-        super(new BorderLayout());
+
+        JPanel m = new JPanel();
 
         //Create the log first, because the action listeners
         //need to refer to it.
@@ -32,54 +33,92 @@ public class SwingStudentBA extends JPanel
 
         //Create a file chooser
         fc = new JFileChooser();
-
-        //Uncomment one of the following lines to try a different
-        //file selection mode.  The first allows just directories
-        //to be selected (and, at least in the Java look and feel,
-        //shown).  The second allows both files and directories
-        //to be selected.  If you leave these lines commented out,
-        //then the default mode (FILES_ONLY) will be used.
-        //
         fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 
         //Create the open button.  We use the image from the JLF
         //Graphics Repository (but we extracted it from the jar).
-        openButton = new JButton("Browse...",
-                createImageIcon("images/Open16.gif"));
-        openButton.addActionListener(this);
+
 
         //Create the save button.  We use the image from the JLF
         //Graphics Repository (but we extracted it from the jar).
-        saveButton = new JButton("Save a File...",
-                createImageIcon("images/Save16.gif"));
-        saveButton.addActionListener(this);
+//        saveButton = new JButton("Save a File...",
+//                createImageIcon("images/Save16.gif"));
+//        saveButton.addActionListener(this);
 
 
         //For layout purposes, put the buttons in a separate panel
         JPanel imgPanel = new JPanel();
         imgPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
         ImageIcon mine = createImageIcon("images/BA4.png");
-        Border paddingBorder = BorderFactory.createEmptyBorder(5, 15, 10, 10);
-        Border buttonBorder = BorderFactory.createEmptyBorder(5, 10, 5, 10);
+
+//        Border buttonBorder = BorderFactory.createEmptyBorder(5, 10, 5, 10);
 
         JLabel label2 = new JLabel(mine);
         label2.setBorder(paddingBorder);
         imgPanel.add(label2);
 
-        JPanel buttonPanel = new JPanel(); //use FlowLayout
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(200,0,0,0));
+//        JPanel buttonPanel = new JPanel(); //use FlowLayout
+//        buttonPanel.setBorder(BorderFactory.createEmptyBorder(200, 0, 0, 0));
 //        openButton.setBorder(buttonBorder);
 //        saveButton.setBorder(buttonBorder);
-        buttonPanel.add(openButton);
-        buttonPanel.add(saveButton);
+//        buttonPanel.add(openButton);
+//        buttonPanel.add(saveButton);
+        TextField a = new TextField();
+        openButton = new JButton("Browse...",
+                createImageIcon("images/Open16.gif"));
+        openButton.addActionListener(this);
+        JPanel inputPanel = MakeTitledBorderPanel("Input File");
+        inputPanel.add(a);
+        inputPanel.add(openButton);
+
+        TextField b = new TextField();
+        openButton = new JButton("Browse...",
+                createImageIcon("images/Open16.gif"));
+        openButton.addActionListener(this);
+        JPanel outputPanel = MakeTitledBorderPanel("Output");
+        outputPanel.add(b);
+        outputPanel.add(openButton);
 
 
         //Add the buttons and the log to this panel.
-        add(imgPanel, BorderLayout.SOUTH);
+        m.setLayout(new BorderLayout());
+        m.add(imgPanel, BorderLayout.SOUTH);
 //        add(logScrollPane, BorderLayout.LINE_START);
-        add(buttonPanel, BorderLayout.CENTER);
+        m.add(inputPanel, BorderLayout.CENTER);
+        m.add(outputPanel, BorderLayout.CENTER);
+
+
+        MakeFrame().add(m);
+
 
     }
+
+    private JFrame MakeFrame() {
+        JFrame frame = new JFrame("SwingStudentBA");
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension dim = tk.getScreenSize();
+        int screenHeight = dim.height;
+        int screenWidth = dim.width;
+        int xPos = (screenWidth / 2) - (frame.getWidth() / 2);
+        int yPos = (screenHeight / 2) - (frame.getHeight() / 2);
+        frame.setLocation(xPos, yPos);
+        frame.setSize(650, 500);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+
+        //Display the window.
+        frame.setVisible(true);
+        return frame;
+    }
+
+    private JPanel MakeTitledBorderPanel(String title) {
+        JPanel pan = new JPanel();
+        pan.setBorder(BorderFactory.createCompoundBorder(paddingBorder, BorderFactory.createTitledBorder(title)));
+        return pan;
+    }
+
 
     public void actionPerformed(ActionEvent e) {
 
@@ -129,43 +168,21 @@ public class SwingStudentBA extends JPanel
      * event dispatch thread.
      */
     private static void createAndShowGUI() {
-        JFrame frame = new JFrame("SwingStudentBA");
-        Toolkit tk = Toolkit.getDefaultToolkit();
-        Dimension dim = tk.getScreenSize();
-        int screenHeight = dim.height;
-        int screenWidth = dim.width;
-        int xPos = (screenWidth / 2) - (frame.getWidth() / 2);
-        int yPos = (screenHeight / 2) - (frame.getHeight() / 2);
-        frame.setLocation(xPos, yPos);
-        //Create and set up the window.
 
-        frame.pack();
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(650, 500);
+        new SwingStudentBA();
 
-
-        // center the jframe on screen
-        frame.setLocationRelativeTo(null);
-
-
-        //Add content to the window.
-        frame.add(new SwingStudentBA());
-
-        //Display the window.
-
-        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
-        SwingUtilities.invokeLater(new Runnable() {
+        Runnable r = new Runnable() {
             public void run() {
                 //Turn off metal's use of bold fonts
                 UIManager.put("swing.boldMetal", Boolean.FALSE);
                 createAndShowGUI();
             }
-        });
+        };
+        SwingUtilities.invokeLater(r);
     }
 }
